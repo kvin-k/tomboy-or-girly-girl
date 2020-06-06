a4f6f898f084c98e427ee7a1f90f0c2aad8204c3
kandavelliVinithKumar <37012080+kandavelliVinithKumar@users.noreply.github.com>
2020-06-06 20:13:23 +0530

    Updates

---
+++ precache-manifest.2dd7ec74cd9c9a4b6c58d527cfacbc3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d77bc425a1ec153ef6245edc9422a29c",
+    "url": "/tomboy-or-girly-girl/index.html"
+  },
+  {
+    "revision": "e88acf69106d00223c7c",
+    "url": "/tomboy-or-girly-girl/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "071f67f72497b568fbce",
+    "url": "/tomboy-or-girly-girl/static/css/main.45ac70bc.chunk.css"
+  },
+  {
+    "revision": "e88acf69106d00223c7c",
+    "url": "/tomboy-or-girly-girl/static/js/2.7e5790d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tomboy-or-girly-girl/static/js/2.7e5790d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "071f67f72497b568fbce",
+    "url": "/tomboy-or-girly-girl/static/js/main.3a53b3bb.chunk.js"
+  },
+  {
+    "revision": "30bcd85d3ec0786e0c98",
+    "url": "/tomboy-or-girly-girl/static/js/runtime-main.68830fb5.js"
+  },
+  {
+    "revision": "33302ac80577fc3b0cb9e9c82f9a2570",
+    "url": "/tomboy-or-girly-girl/static/media/GLAMOURGIRL.33302ac8.TTF"
+  }
+]);
